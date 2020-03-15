5d453693167fed19f85c81127b5c10a99cd5ad79
Jesen Tanadi <mail@jesentanadi.com>
2020-03-15 17:16:19 -0400

    migrate to restana

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const restana = require("restana");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const NodeCache = require("node-cache");
 
 const getRepoContents = require("./utils/getRepoContents");
 
-const app = express();
+const app = restana();
 const cache = new NodeCache();
 const port = process.env.PORT || 3000;
 
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.redirect(301, "https://github.com/raa-tools/operator");
+  res.send("", 301, { Location: "https://github.com/raa-tools/operator" });
 });
 
 // This endpoint is called only when we push to the GH repo,
@@ -22,7 +22,7 @@ app.post("/api/postreceive", async (req, res) => {
   const { name } = req.body.repository;
   const repoDirs = await getRepoContents(name);
   cache.set(name, repoDirs);
-  res.sendStatus(204);
+  res.send(204);
 });
 
 app.get("/api/repo/:name", async (req, res) => {
@@ -31,10 +31,16 @@ app.get("/api/repo/:name", async (req, res) => {
   // Use cached directories if available
   let repoDirs = cache.get(name);
   if (!repoDirs) {
-    repoDirs = await getRepoContents(name);
+    try {
+      repoDirs = await getRepoContents(name);
+    } catch (e) {
+      res.send(e);
+    }
   }
+
   res.send(repoDirs);
   cache.set(name, repoDirs);
 });
 
-app.listen(port, console.log(`Listening on port ${port}`));
+app.start(port).then(() => console.log(`Listening on port ${port}`));
+
