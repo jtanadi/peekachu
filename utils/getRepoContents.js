601d0e4139a66547fd017b1180859626d2cfc048
Jesen Tanadi <mail@jesentanadi.com>
2020-03-15 17:16:50 -0400

    move error handling to server; function throws error

--- utils/getRepoContents.js
+++ utils/getRepoContents.js
@@ -1,29 +1,35 @@
 const { api, authHeader } = require("./githubAPI");
 const buildTree = require("./buildTree");
+const makeError = require("./makeError");
 const request = require("./request");
 
 module.exports = async repo => {
-  try {
-    const commits = await request(api + `/${repo}/commits`, {
-      headers: authHeader
-    });
-    const commitSHA = commits[0].sha;
+  const commits = await request(api + `/${repo}/commits`, {
+    headers: authHeader
+  });
 
-    const { tree } = await request(
-      api + `/${repo}/git/trees/${commitSHA}?recursive=true`,
-      {
-        headers: authHeader
-      }
-    );
-
-    return tree.reduce((accum, _tree) => {
-      // Disregard directories and hidden files
-      if (_tree.type !== "blob" || _tree.path.startsWith(".")) return accum;
-
-      let path = _tree.path.split("/");
-      return buildTree(accum, path);
-    }, {});
-  } catch (e) {
-    console.log(e);
+  if (!commits[0]) {
+    throw makeError(404, `No commits found in repo '${repo}'.`);
   }
+
+  const commitSHA = commits[0].sha;
+
+  const { tree } = await request(
+    api + `/${repo}/git/trees/${commitSHA}?recursive=true`,
+    {
+      headers: authHeader
+    }
+  );
+
+  if (!tree) {
+    throw makeError(500, `Problem getting tree in repo ${repo}`);
+  }
+
+  return tree.reduce((accum, _tree) => {
+    // Disregard directories and hidden files
+    if (_tree.type !== "blob" || _tree.path.startsWith(".")) return accum;
+
+    let path = _tree.path.split("/");
+    return buildTree(accum, path);
+  }, {});
 };
